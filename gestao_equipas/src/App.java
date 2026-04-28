8f4fb4ba54275f54ace5cf5bfc947eb5e874eaca
Rompante <a14866@oficina.pt>
2026-04-28 07:46:38 +0000

    criação da viatura

--- gestao_equipas/src/App.java
+++ gestao_equipas/src/App.java
@@ -2,11 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Viatura viatura1 = new Viatura();
-        viatura1.setTipoCarro("Ford");
+        viatura1.setTipoCarro("Carro de Passeio");
         viatura1.setNumRodas(4);
         viatura1.setVelMaxima(120);
 
         viatura1.mostraInfo();
-
     }
 }
